cb08648fa597abe7303a4c6ba3246e1049a653da
bOkO <bOkO@abv.bg>
2026-02-22 02:24:57 +0200

    Fix sort select typing

--- components/FiltersSidebar.tsx
+++ components/FiltersSidebar.tsx
@@ -114,7 +114,7 @@ export default function FiltersSidebar({
           <select
             className="mt-2 w-full rounded-xl border border-ink/10 bg-white px-3 py-2"
             value={sort}
-            onChange={(event) => setSort(event.target.value)}
+            onChange={(event) => setSort(event.target.value as "soonest" | "curated" | "nearest")}
           >
             <option value="soonest">Soonest</option>
             <option value="curated">Curated</option>

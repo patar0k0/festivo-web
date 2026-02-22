a3b80fb83a9122ba41fab8d5967af0c6369c65c2
bOkO <bOkO@abv.bg>
2026-02-22 21:05:04 +0200

    new site fix

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -1,3 +1,16 @@
-export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">{children}</div>;
+import * as React from "react";
+
+type Props = React.HTMLAttributes<HTMLDivElement> & {
+  children: React.ReactNode;
+};
+
+export default function Container({ className = "", children, ...props }: Props) {
+  return (
+    <div
+      className={`mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 ${className}`.trim()}
+      {...props}
+    >
+      {children}
+    </div>
+  );
 }

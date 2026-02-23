02c63b898cb3a0c6e30f48807dd4205a699e96bf
bOkO <bOkO@abv.bg>
2026-02-23 02:38:26 +0200

    aple 11

---
+++ scripts/spawn-log.js
@@ -0,0 +1,47 @@
+const childProcess = require("child_process");
+
+const originalSpawn = childProcess.spawn;
+const originalSpawnSync = childProcess.spawnSync;
+
+console.error("[spawn-log] loaded", process.pid);
+
+function formatArgs(args) {
+  if (!Array.isArray(args)) return "";
+  return args.map((arg) => String(arg)).join(" ");
+}
+
+function logSpawn(command, args, options) {
+  const cwd = (options && options.cwd) || process.cwd();
+  const argText = formatArgs(args);
+  const cmdText = [command, argText].filter(Boolean).join(" ");
+  process.stderr.write(`[spawn] ${cmdText} | cwd=${cwd}\n`);
+}
+
+childProcess.spawn = function patchedSpawn(command, args, options) {
+  logSpawn(command, args, options);
+  const child = originalSpawn.call(childProcess, command, args, options);
+  if (child && typeof child.on === "function") {
+    child.on("error", (err) => {
+      process.stderr.write(
+        `[spawn-error] ${command} ${err && err.errno} ${err && err.code} ${err && err.syscall} ${
+          err && err.message
+        }\n`
+      );
+    });
+  }
+  return child;
+};
+
+childProcess.spawnSync = function patchedSpawnSync(command, args, options) {
+  logSpawn(command, args, options);
+  const result = originalSpawnSync.call(childProcess, command, args, options);
+  if (result && result.error) {
+    const err = result.error;
+    process.stderr.write(
+      `[spawn-error] ${command} ${err && err.errno} ${err && err.code} ${err && err.syscall} ${
+        err && err.message
+      }\n`
+    );
+  }
+  return result;
+};
